c41f4bef45ed86f7cfac3b10e4ddfadcd63b4892
Lilla Doughty <lilla.e.j.du@gmail.com>
2026-07-09 16:23:49 +0200

    Señales computadas + intro a pipes

--- src/app/pages/hero/hero-page.ts
+++ src/app/pages/hero/hero-page.ts
@@ -1,20 +1,37 @@
-import { Component, signal } from "@angular/core";
+//* Una señal computada es una señal común de solo lectura (read-only) cuyos valores derivan de otras señales.
+//* Por lo tanto, pueden cambiar si sus dependencias (las señales) cambian.
+//* A las señales normales (writable signals) puedes asignarle valores (por ejemplo, el .set() )
+//* Pero las señales computadas solo cogen los valores de sus dependencias (las señales que contiene)
+
+import { UpperCasePipe } from "@angular/common";
+import { Component, computed, signal } from "@angular/core";
 
 
 @Component({
     templateUrl: './hero-page.html',
+    imports: [UpperCasePipe], //Importamos el pipe para utilizar el uppercase en el html
 })
 export class HeroPageComponent {
 
     name = signal('Ironman');
     age = signal(45);
+    // newHero = 'Spiderman';
+    // newAge = 22;
 
-    newHero = 'Spiderman';
-    newAge = 22;
+    //* Señal computada
+    heroDescription = computed(() => {
+        const description = `${ this.name() } - ${ this.age() }`;
+        return description;
+    })
 
-    getHeroDescription() {
-        return `${ this.name() } - ${ this.age() }`;
-    }
+    //* Solución original a la tarea
+    // getHeroDescription() {
+    //     return `${ this.name() } - ${ this.age() }`;
+    // }
+
+    //* Señal computada
+    capitalizedName = computed( () => this.name().toLocaleUpperCase())
+
 
     changeHero() {
         //*Mi solución a la tarea - funciona
