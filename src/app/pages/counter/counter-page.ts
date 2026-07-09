c25eb934f44c5904c9a96f27c66c4a4b24482210
Lilla Doughty <lilla.e.j.du@gmail.com>
2026-07-09 13:58:54 +0200

    Nuevo componente + Señales

---
+++ src/app/pages/counter/counter-page.ts
@@ -0,0 +1,30 @@
+import { Component, signal } from "@angular/core";
+
+@Component({
+    templateUrl: './counter-page.html',
+    styleUrls: ['./counter-page.css'],
+})
+export class CounterPageComponent {
+
+    counter = 10;
+    counterSignal = signal( 10 ); //! --> Esto es una señal
+
+    increaseBy( value: number ) {
+        this.counter += value; 
+
+        // this.counterSignal.set( this.counterSignal() + value ); 
+        //* Con esto hacemos el mismo efecto de sumar y restar el valor que el del counter. Sin embargo. 
+        //* con el set RESETEAMOS totalmente el valor original. Para mantener el valor original ç
+        //*  utilizamos el update():
+        this.counterSignal.update( (current) => current + value );
+
+    }
+
+    resetCounter( ) {
+        this.counter = 0;
+        this.counterSignal.set(0); //*Con las señales podemos RESETEAR el valor con el .set()
+    }
+
+
+}
+
