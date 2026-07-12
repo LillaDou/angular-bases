75dbdc9ddeb620ae6f7c26c4ff57f8f6604b0acb
Lilla Doughty <lilla.e.j.du@gmail.com>
2026-07-12 20:20:17 +0200

    @for

--- src/app/pages/dragonball/dragonball-page.ts
+++ src/app/pages/dragonball/dragonball-page.ts
@@ -1,7 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+
+
+interface Character {
+  id: number; 
+  name: string; 
+  power: number;
+}
+
 
 @Component({
   imports: [],
   templateUrl: './dragonball-page.html',
 })
-export class DragonballPageComponent {}
+export class DragonballPageComponent {
+
+//La señal es de tipo Character con un arreglo
+  characters = signal<Character[]>( [
+    { id: 1, name: 'Goku', power: 9001 },
+    { id: 2, name: 'Vegeta', power: 8000 },
+    { id: 3, name: 'Piccolo', power: 3000 },
+  ] ); 
+
+  
+
+}
