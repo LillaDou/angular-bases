fba8056e0460d58e9e586b9feb87d88e43017fa9
Lilla Doughty <lilla.e.j.du@gmail.com>
2026-07-18 17:44:32 +0200

    Tarea de inputs

--- src/app/pages/dragonball/dragonball-page.ts
+++ src/app/pages/dragonball/dragonball-page.ts
@@ -1,5 +1,4 @@
 import { Component, computed, signal } from '@angular/core';
-import { NgClass } from "../../../../node_modules/@angular/common/types/_common_module-chunk";
 
 
 interface Character {
@@ -11,11 +10,13 @@ interface Character {
 
 @Component({
   imports: [
-    // NgClass
   ],
   templateUrl: './dragonball-page.html',
-})
+  })
 export class DragonballPageComponent {
+
+  name = signal('Gohan');
+  power = signal(100);
 
 //La señal es de tipo Character con un arreglo
   characters = signal<Character[]>( [
@@ -33,4 +34,31 @@ export class DragonballPageComponent {
   //   }
   // })
 
+  addCharacter() {
+    if ( !this.name() || !this.power || this.power() <= 0 ) {
+      return;
+    }
+
+    const newCharacter: Character = {
+      id: this.characters().length + 1,
+      name: this.name(),
+      power: this.power(),
+    };
+
+    // this.characters().push(newCharacter);
+    // Esta es una forma de poder añadir el personaje nuevo a nuestro arreglo de characters. Funciona. Sin embargo,
+    // no deberíamos usar el .push() cuando estamos cambiando el valor de una señal. 
+
+    this.characters.update( (list) => [... list, newCharacter] );
+    // Utilizamos el update porque queremos actualizar el valor de la señal. Dentro, añadimos la función que retorna
+    // nuestra lista original + el newCharacter
+
+    this.resetFields();
+  }
+
+  //Creamos esta función para resetear las cajas de texto después de añadir un personaje nuevo
+  resetFields() {
+    this.name.set('');
+    this.power.set(0);
+  }
 }
