0ef23281cfb5315faeb4708d297d062ca7461460
Lilla Doughty <lilla.e.j.du@gmail.com>
2026-07-19 17:24:18 +0200

    Efectos y localStorage

--- src/app/services/dragonball.service.ts
+++ src/app/services/dragonball.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, signal } from '@angular/core';
+import { effect, Injectable, signal } from '@angular/core';
 import { Character } from '../interfaces/character.interface';
 
 
@@ -13,9 +13,24 @@ export class DragonballService {
     { id: 2, name: 'Vegeta', power: 8000 },
   ] ); 
 
+
+  //Un efecto nos va a permitir disparar un efecto o "acción secundaria"
+  //Es una función que recibe un callback que vamos a querer disparar cuando algo funciona. 
+  //En este caso, necesitamos un efecto que se dispare siempre que creemos un personaje nuevo, guardando
+  //la información en el localStorage
+  //Es importante siempre seguir las buenas prácticas. En este caso, es importante que cada efecto tenga solo una tarea.
+  //Como por ejemplo, guardar en el localStorage. Pero para leerlo,borrarlo... deberemos crear otro efecto. 
+  saveToLocalStorage = effect( () => {
+    localStorage.setItem('characters', JSON.stringify( this.characters() ));
+  } );
+  //* Hemos definido cómo guardar la información en el localStorage. Pero si cargamos la página teniendo solo
+  //* esta sintaxis, se perderá la información.  
+
+
   addCharacter( character: Character) {
     this.characters.update( (list) => [...list, character] )
   }
+
   
 }
 
@@ -23,3 +38,6 @@ export class DragonballService {
 //! Los servicios son clases que tienen un propósito particular. 
 //! Los servicios trabajan con Dependency Injections o DI, que nos permite utilizar la clase como un 
 //! Singleton. Es decir, la idea de los servicios es que funcionen como un lugar centralizado de nuestra información.
+
+//? El localStorage necesita siempre de un key y un value. El Key es el nombre identificativo, mientras que el value
+//? es el valor que tendrá en formato STRING. 
