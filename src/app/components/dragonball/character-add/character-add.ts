6ecfd7da9e12f13f48568302f4ae28757c37387a
Lilla Doughty <lilla.e.j.du@gmail.com>
2026-07-19 14:58:53 +0200

    Emitir valores con outputs

--- src/app/components/dragonball/character-add/character-add.ts
+++ src/app/components/dragonball/character-add/character-add.ts
@@ -1,4 +1,4 @@
-import {  Component, signal } from '@angular/core';
+import {  Component, output, signal } from '@angular/core';
 import { Character } from '../../../interfaces/character.interface';
 
 @Component({
@@ -8,7 +8,10 @@ import { Character } from '../../../interfaces/character.interface';
 export class CharacterAdd {
 
   name = signal('');
-  power = signal(0)
+  power = signal(0);
+
+  //Vamos a emitir un personaje desde este evento
+  newCharacter = output<Character>();
 
   addCharacter() {
     if ( !this.name() || !this.power || this.power() <= 0 ) {
@@ -17,13 +20,13 @@ export class CharacterAdd {
 
     const newCharacter: Character = {
       // id: this.characters().length + 1,
-      id: 10000,
+      id: Math.floor(Math.random() * 1000),
       name: this.name(),
       power: this.power(),
     };
 
     // this.characters.update( (list) => [... list, newCharacter] );
-    console.log({newCharacter});
+    this.newCharacter.emit( newCharacter );
     this.resetFields();  
   }
   
--- src/app/pages/dragonball-super/dragonball-super-page.ts
+++ src/app/pages/dragonball-super/dragonball-super-page.ts
@@ -28,21 +28,8 @@ export class DragonballSuperPageComponent {
     { id: 2, name: 'Vegeta', power: 8000 },
   ] ); 
 
-  addCharacter() {
-    if ( !this.name() || !this.power || this.power() <= 0 ) {
-      return;
-    }
-
-    const newCharacter: Character = {
-      // id: this.characters().length + 1,
-      id: 10000,
-      name: this.name(),
-      power: this.power(),
-    };
-
-    // this.characters.update( (list) => [... list, newCharacter] );
-    console.log({newCharacter});
-    this.resetFields();
+  addCharacter( character: Character) {
+    this.characters.update( (list) => [...list, character] )
   }
 
   resetFields() {
